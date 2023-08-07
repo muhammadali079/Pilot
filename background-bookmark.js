546f6fd3ba70c5e5001bd0fd264af93e9d6230da
Muhammad Ali <140754196+muhammadali079@users.noreply.github.com>
2023-08-07 19:29:57 +0500

    Update background-bookmark.js

--- background-bookmark.js
+++ background-bookmark.js
@@ -1,33 +1,33 @@
-function skipAds(videoElement) {
-  if (videoElement && !videoElement.paused) {
-    const currentTime = videoElement.currentTime;
-    const duration = videoElement.duration;
-    const adDurationThreshold = 30; 
-    if (duration <= adDurationThreshold) {
-      videoElement.currentTime = duration;
-    }
-  }
-}
+// function skipAds(videoElement) {
+//   if (videoElement && !videoElement.paused) {
+//     const currentTime = videoElement.currentTime;
+//     const duration = videoElement.duration;
+//     const adDurationThreshold = 30; 
+//     if (duration <= adDurationThreshold) {
+//       videoElement.currentTime = duration;
+//     }
+//   }
+// }
 
-function handleVideoAdded(videoElement) {
-  skipAds(videoElement);
-}
+// function handleVideoAdded(videoElement) {
+//   skipAds(videoElement);
+// }
 
-function handleDOMMutation(mutationsList, observer) {
-  for (const mutation of mutationsList) {
-    if (mutation.type === 'childList') {
-      for (const addedNode of mutation.addedNodes) {
-        if (addedNode.tagName && addedNode.tagName.toLowerCase() === 'video') {
-          handleVideoAdded(addedNode);
-        }
-      }
-    }
-  }
-}
+// function handleDOMMutation(mutationsList, observer) {
+//   for (const mutation of mutationsList) {
+//     if (mutation.type === 'childList') {
+//       for (const addedNode of mutation.addedNodes) {
+//         if (addedNode.tagName && addedNode.tagName.toLowerCase() === 'video') {
+//           handleVideoAdded(addedNode);
+//         }
+//       }
+//     }
+//   }
+// }
 
-const observerConfig = { childList: true, subtree: true };
-const observer = new MutationObserver(handleDOMMutation);
-observer.observe(document.body, observerConfig);
+// const observerConfig = { childList: true, subtree: true };
+// const observer = new MutationObserver(handleDOMMutation);
+// observer.observe(document.body, observerConfig);
 
 
 chrome.tabs.onUpdated.addListener((tabId, tab) => {
